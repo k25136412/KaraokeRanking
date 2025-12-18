2fce8b2a4ce6e20ea7b2568609c14c7de9cadb50
k25136412 <k25136412@gmail.com>
2025-12-18 14:05:12 +0000

    1

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,5 +1,12 @@
 {pkgs}: {
   channel = "stable-24.05";
+  # ここに環境変数を設定します
+  env = {
+    # 自動認証ポップアップを無効化（空文字にする）
+    GIT_ASKPASS = "";
+    # ターミナルでのパスワード入力を強制的に有効化
+    GIT_TERMINAL_PROMPT = "1";
+  };
   packages = [
     pkgs.nodejs_20
   ];
